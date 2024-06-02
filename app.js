fac3023c859d0249c5ab78fee986d6b528b92f3b
maihunsandip <skole472@gmail.com>
2024-06-02 16:16:41 +0530

    implimented Register rout logic.

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect("mongodb://localhost:27017/Camp-Scape", {
   useNewUrlParser: true,
@@ -68,8 +69,9 @@ app.get('/fakeuser', async (req, res) => {
   res.send(reguser);
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
   res.render('home')
