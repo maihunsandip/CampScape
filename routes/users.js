58a617874da5873346e4749eabc74891d19244e1
maihunsandip <skole472@gmail.com>
2024-06-02 18:02:54 +0530

    implemented Login & Logout logic

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,45 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync (async(req, res) => {
+router.post('/register', catchAsync(async (req, res) => {
     try {
-        const {username, email, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('success', 'Welcome to CampScape!');
-    res.redirect('/campgrounds')
+        const { username, email, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash('success', 'Welcome to CampScape!');
+        res.redirect('/campgrounds')
     }
     catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
 
 module.exports = router;
